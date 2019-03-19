affb453a12c50dc5651bf1b9d0c47d7ec985c039
Michal Czaplinski <mmczaplinski@gmail.com>
2019-03-19 17:04:45 -0500

    add router

---
+++ src/dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Dashboard() {
+  return (
+    <div>
+      dashboard
+      <div>
+        <Link to="/">home </Link>
+      </div>
+    </div>
+  );
+}
